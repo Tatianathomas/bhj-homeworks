bca1c4586798f93b620a2607cc2ce205ff3ac620
Tatiana Thomas <murzik555@inbox.ru>
2020-06-30 21:02:17 +0500

    V

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -2,27 +2,11 @@ const time = function () {
     const count = document.getElementById('timer');
     count.textContent--;
     if (count.textContent < 1) {
-        alert('Вы победили в конкурсе!')
+        alert('Вы победили в конкурсе!');
+        clearInterval(interval);
     } 
 } 
 const interval = setInterval(time, 1000);
-setInterval(time, 1000);
-clearInterval(interval);
 
 
-
-
-/*
-    function watch (hour, min, sec) {
-    sec--;
-    if (sec < 0) {
-        sec = 59; min--
-    }
-    else {min = min};
-    if (sec < 10) {
-        sec = `0` + sec
-    } 
-    if 
-}
-    */
 
